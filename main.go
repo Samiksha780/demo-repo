3f41f9e424eef3db0f9e9ccd804103f4e5ffdca5
Samiksha780 <80950442+Samiksha780@users.noreply.github.com>
2021-03-19 16:03:05 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ func main() {
 
 	fmt.Printf("Hello World")
 	fmt.Printf("My first local update")
-	fmt.Printf("My second online update is done as of now")
+	fmt.Printf("My second pdate is done as of now")
 }
